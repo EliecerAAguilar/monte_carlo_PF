1c4be4ffb26f67339f473c8e02d78ad3904567bb
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-12-08 15:19:02 -0500

    ejemplo #5 hint typing

--- CodigoPY/ejemplo05.py
+++ CodigoPY/ejemplo05.py
@@ -1,47 +1,75 @@
 from tabulate import tabulate
 import math
 import random
+from typing import List
 
-def calculoPi(long,n):
-    data = []
-    aciertos = 0
-    for i in range(n):
-        r1=random.uniform(0,1)
-        x=r1*(long/2.0)
-        r2=random.uniform(0,1)
-        y=(long/2.0)*math.sin(180*r2)
-        if(x<=y):
-            aciertos+=1
-            textAcierto = '\u2713'
-        else:
-            textAcierto = '\u274C'
-        data.append((r1,x,r2,y,textAcierto))
-    present(data)
-    vpi=n/aciertos
-    print('N: {}'.format(n))
-    print('Aciertos: {}'.format(aciertos))
-    print('Valor aproximado de PI: {}'.format(vpi))
+class Ejemplo05:
+    """Clase para el desarrollo del Ejemplo #5"""
+    def calculoPi(self,long: int,n: int) -> None:
+        """
+        Metodo para realizar los calculos de la simulacion
+        :param long: longitud de las agujas en cm
+        :type long: int
+        :param n: cantidad de lanzamientos a realizar
+        :type n: int
+        :return: Nada
+        :rtype: None
+        """
+        data: List[tuple] = []
+        aciertos: int = 0
+        for i in range(n):
+            r1: float = random.uniform(0,1)
+            x: float = r1*(long/2.0)
+            r2: float = random.uniform(0,1)
+            y: float = (long/2.0)*math.sin(180*r2)
 
-def present(data):
-    headers = [ 'r1',
-                'x', 
-                'r2',
-                'Pi*r2',
-                'Y', 
-                'Acierto'
-                ]
+            if x<=y:
+                aciertos+=1
+                textAcierto = '\u2713'
+            else:
+                textAcierto = '\u274C'
 
-    print(tabulate(data, headers=headers, showindex=True))
+            data.append((r1,x,r2,y,textAcierto))
 
-def main():
-    #Lectura de datos
-    print('CALCULO DEL VALOR DE PI')
-    long = int(input('Introduzca longitud de la aguja(cms): '))
-    n = int(input('Introduzca cantidad de lanzamientos: '))
-    print('-------------------------')
-    print('CALCULO DEL VALOR DE PI')
-    print('Longitud de la Aguja (cms) ==> ',long)
-    print('Número de lanzamientos (cms) ==> ',n)
-    calculoPi(long,n)
+        self.present(data)
 
-main()
+        vpi: float = n/aciertos
+
+        print(f'N: {n}')
+        print(f'Aciertos: {aciertos}')
+        print(f'Valor aproximado de PI: {vpi}')
+
+    def present(self,data: List[tuple]) ->None:
+        """
+        Imprime los valores de la simulacion en formato de tabla
+        :param data: lista con los valores a mostrar
+        :type data: List[tuple]
+        :return: Nada
+        :rtype None
+        """
+        headers = [ 'r1',
+                    'x',
+                    'r2',
+                    'Pi*r2',
+                    'Y',
+                    'Acierto'
+                    ]
+
+        print(tabulate(data, headers=headers, showindex=True))
+
+    def run(self)-> None:
+        """
+        Metodo para ejecutar el Ejemplo #5
+        Pide los datos para ejecutar la simulacion por medio de la aguja de buffon
+        :return:
+        :rtype None
+        """
+        print('CALCULO DEL VALOR DE PI')
+        long = int(input('Introduzca longitud de la aguja(cms): '))
+        n = int(input('Introduzca cantidad de lanzamientos: '))
+        print('-------------------------')
+        print('CALCULO DEL VALOR DE PI')
+        print('Longitud de la Aguja (cms) ==> ',long)
+        print('Número de lanzamientos (cms) ==> ',n)
+        self.calculoPi(long,n)
+
