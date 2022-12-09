62d59dc27455d1f8721da24cafa2b2cdad8eefd2
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-12-09 12:24:56 -0500

    hint typing ejemplo #9, optimizacion de algoritmo verificacion victria

--- CodigoPY/ejemplo09.py
+++ CodigoPY/ejemplo09.py
@@ -1,44 +1,72 @@
 import random
-import math
 
-def singlecrap():
-    dice1 = random.randint(1,6)
-    dice2 = random.randint(1,6)
 
-    sum1 = dice1 + dice2
-    if (sum1 == 7 or sum1 == 11):  #Condicion de victoria en la primera tirada
-        return 1
-    elif (sum1 == 2 or sum1 == 3 or sum1 == 12):  #Condicion de perdida en la primera tirada
-        return 0
-    else: #Condicion de seguir tirando dados.
-        foo = True
-        while(foo):
-            dice1 = random.randint(1,6)
-            dice2 = random.randint(1,6)
-            sum2 = dice1 + dice2
-            if(sum2 == sum1):
-                foo = False
+class Ejemplo09:
+    """Clase para el desarrollo del Ejemplo #9"""
+    def singlecrap(self) -> int:
+        """
+        Metodo para verificar las condiciones de victoria y perdida
+        :return: valor entero que verifica la victoria o derrota
+        :rtype int
+        """
+
+        condicion = {
+            7: 1,  # Winning condition
+            11: 1,  # Winning condition
+            2: 0,  # Losing condition
+            3: 0,  # Losing condition
+            12: 0  # Losing condition
+        }
+
+        dice_roll_1 = random.randint(1, 6)
+        dice_roll_2 = random.randint(1, 6)
+
+
+        sum1 = dice_roll_1 + dice_roll_2
+
+        # verificar la condicion de victorio o perdida en la primera tirada de dados
+        if sum1 in condicion:
+            return condicion[sum1]
+
+        # seguir tirando los dados si la condicion de victoria/ derrota no se cumple la primera vez
+        for _ in range(100):
+            dice_roll_1 = random.randint(1, 6)
+            dice_roll_2 = random.randint(1, 6)
+            sum2 = dice_roll_1 + dice_roll_2
+
+            if sum2 == sum1:
                 return 1
-            elif(sum2==7):
-                foo = False
-                return 0 
+            elif sum2 == 7:
+                return 0
 
+        # condicion de empate, en el caso de darse cuando se tiran muchas veces sin alcanzar la condicion
+        # de victoria o derrota
+        return -1
 
+    def crap(self, n: int) -> int:
+        """
+        Metodo para la ejecucion de la simulacion
+        :param n: cantidad de veces a correr el juego
+        :type n: int
+        :return: cantidad de veces que gano
+        :rtype int
+        """
+        wins: int = 0
+        for i in range(n):
+            wins = wins + self.singlecrap()
+        return wins
 
-def crap(n):
-    wins = 0
-    for i in range(n):
-        wins = wins + singlecrap()
-    return wins
+    def run(self) -> None:
+        """
+        Metodo para ejecutar la simulacion del Ejemplo #9
+        :return: Nada
+        :rtype None
+        """
+        pTeorica = 0.49228 #Probabilidad real de ganar el juego
+        n: int =  int(input('Cuantas veces desea correr el juego (Preferiblemente mas de 10 000): '))
+        wins: int = self.crap(n)
+        pExperimental: float = wins/n #Probabilidad experimental(simulada) de ganar el juego
+        error: float = abs((pExperimental-pTeorica)/pTeorica) * 100
+        print(f'La probabilidad de ganar el juego es: {pExperimental}')
+        print(f'Teoricamente esta probabilidad es de {pTeorica} donde comparando con la Simulacion tenemos un error de {error:.4f}%')
 
-def run(): 
-    #Numero de repeticiones del juego
-    pTeorica = 0.49228 #Probabilidad real de ganar el juego 
-    n =  int(input('Cuantas veces desea correr el juego (Preferiblemente mas de 10 000): '))
-    wins = crap(n)
-    pExperimental = wins/n #Probabilidad experimental(simulada) de ganar el juego
-    error = abs((pExperimental-pTeorica)/pTeorica) *100
-    print('La probabilidad de ganar el juego es: {}'.format(pExperimental))
-    print('Teoricamente esta probabilidad es de {0} donde comparando con la Simulacion tenemos un error de {1:.4f}%'.format(pTeorica, error))
-
-run()
