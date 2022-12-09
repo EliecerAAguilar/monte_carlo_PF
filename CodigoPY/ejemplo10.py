1caebe418a3f97d6f3801efcd989d3045676c063
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-12-09 13:06:15 -0500

    ejemplo

--- CodigoPY/ejemplo10.py
+++ CodigoPY/ejemplo10.py
@@ -1,66 +1,81 @@
 import random
 import pandas as pd
+from typing import List
 
+class Ejemplo10:
+    """Clase para el Ejemplo #10"""
+    def pennysgame(self, p1choice: str, p2choice: str) -> int:
 
-def pennysgame(p1choice, p2choice):
-    
-    coinOptions = ['T', 'H']
-    coin = []
-    #Simulamos los primeros 3 lanzamientos
-    for i in range(3): 
-        coin.append(coinOptions[random.randint(0,1)])
+        coin_options = ['T', 'H']
 
-    doo = True
-    while(doo):
-        if(list(p1choice) == coin):
-            doo = False
-            return 1
-        elif (list(p2choice) == coin):
-            doo = False
-            return 0
-        else: 
-            coin = coin[1:]
-            coin.append(coinOptions[random.randint(0,1)])
+        coin: List[str] = []
+        for _ in range(3):
+            coin.append(coin_options[random.randint(0, 1)])
 
-def games(n):
-    choices = ['TTT', 'TTH', 'THT', 'THH', 'HTT', 'HTH', 'HHT', 'HHH'] #H: Cara | T: Sello
-    tableWins = []
-    for choice1 in choices:
-        Semiwins = {}
-        for choice2 in choices:
-            wins = 0
-            if choice1 != choice2: 
-                p1choice = choice1
-                p2choice = choice2 
-                #Se crea la siguiente condicion para que no se repitan los n gramas.
-                for i in range(n): 
-                    vic = pennysgame(p1choice, p2choice)
-                    if vic == 1: 
-                        wins +=  1
-                Semiwins[''.join(choice1)+'/'+''.join(choice2)] = wins
+        for _ in range(100):
+            if coin == list(p1choice):
+                return 1
+            elif coin == list(p2choice):
+                return 0
             else:
-                Semiwins[''.join(choice1)+'/'+''.join(choice2)] = 0
-        tableWins.append(Semiwins)
-    return tableWins
+                coin = coin[1:]
+                coin.append(coin_options[random.randint(0, 1)])
+        # condicion de empate
+        return -1
 
-def present(wins, n):
-    choices = ['TTT', 'TTH', 'THT', 'THH', 'HTT', 'HTH', 'HHT', 'HHH'] #H: Cara | T: Sello
-    data = []
-    for group in wins:
-        dataElem = []
-        for value in group.values():
-            dataElem.append(value/n)
-        data.append(dataElem)
-    df = pd.DataFrame(data, columns=choices, index=choices)
-    print('Probabilidades de victoria del jugador 1 (filas: 3-grama de 1 | columnas: 3-grama de 2)')
-    print(df)
+    def games(self, n: int) -> List[dict]:
+        """
+        metodo para ejecutar el juego
+        :param n: cantidad de veces a ejecutar el juego
+        :return: lista de diccionarios con los ganadores
+        :rtype List[dict]
+        """
+        choices: List[str] = ['TTT', 'TTH', 'THT', 'THH', 'HTT', 'HTH', 'HHT', 'HHH'] #H: Cara | T: Sello
+        tableWins: List[dict] = []
+        for choice1 in choices:
+            Semiwins: dict = {}
+            for choice2 in choices:
+                wins = 0
+                if choice1 != choice2:
+                    p1choice: str = choice1
+                    p2choice: str = choice2
+                    #Se crea la siguiente condicion para que no se repitan los n gramas.
+                    for i in range(n):
+                        vic = self.pennysgame(p1choice, p2choice)
+                        if vic == 1:
+                            wins +=  1
+                    Semiwins[''.join(choice1)+'/'+''.join(choice2)] = wins
+                else:
+                    Semiwins[''.join(choice1)+'/'+''.join(choice2)] = 0
+            tableWins.append(Semiwins)
+        return tableWins
 
+    def present(self, wins: List[dict], n: int) -> None:
+        """
+        Metodo para imprimir en pantalla los valores de la simulacion del juego
+        :param wins: lista de diccionarios con la cantidad de veces que gana cada jugador
+        :type wins: List[dict]
+        :param n: cantidad de veces a simular este juego
+        :type n: int
+        :return:
+        """
+        choices = ['TTT', 'TTH', 'THT', 'THH', 'HTT', 'HTH', 'HHT', 'HHH'] #H: Cara | T: Sello
+        data = []
+        for group in wins:
+            dataElem = []
+            for value in group.values():
+                dataElem.append(value/n)
+            data.append(dataElem)
+        df = pd.DataFrame(data, columns=choices, index=choices)
+        print('Probabilidades de victoria del jugador 1 (filas: 3-grama de 1 | columnas: 3-grama de 2)')
+        print(df)
 
-
-def run():
-    n = int(input('Ingresa la cantidad de juegos que deseas simular (mayor a 1000): ')) #Cantidad de juegos
-    wins = games(n) #Diccionarion de ganadas de cada uno
-    present(wins,n)
-
-
-run()
+    def run(self) -> None:
+        """
+        Metodo para ejecutar la simulacion del juego de penny
+        :return: Nada
+        :rtype None
+        """
+        n: int = int(input('Ingresa la cantidad de juegos que deseas simular (mayor a 1000): ')) #Cantidad de juegos
+        wins:List[dict] = self.games(n) #Diccionarion de ganadas de cada uno
+        self.present(wins,n)
